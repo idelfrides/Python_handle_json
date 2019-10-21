0aa471de7038e55bc51945b69a740e2fc0b8f487
Idelfrides Jorge <idelfridesjorge@alu.ufc.br>
2019-10-20 22:39:47 -0300

    Update __init__.py

--- JsonHandlerPackage/jsonlibrary/__init__.py
+++ JsonHandlerPackage/jsonlibrary/__init__.py
@@ -1,9 +1,1 @@
 
-"""
-from .CreateJson import CreateJson as creat_j
-from . import ReadJson as read_j
-from . import UpdateJson as update_j
-from . import DeleteJson as delete_j
-from . import AddRecord2Json as add_r
-
-"""
